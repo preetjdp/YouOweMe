f84bbe998e585207ad4bd2cdf3654796d4f154e5
Preet Parekh <preetjdp123@gmail.com>
2020-03-10 12:40:18 +0530

    use Brightness in YOMSpinner

--- mobileApp/lib/ui/Abstractions/yomSpinner.dart
+++ mobileApp/lib/ui/Abstractions/yomSpinner.dart
@@ -2,10 +2,16 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class YOMSpinner extends StatelessWidget {
-  final Color spinnerColor;
-  YOMSpinner({this.spinnerColor = Colors.white});
+  final Brightness brightness;
+  YOMSpinner({this.brightness = Brightness.dark});
   @override
   Widget build(BuildContext context) {
+    Color spinnerColor;
+    if(brightness == Brightness.light) {
+      spinnerColor = Colors.white;
+    } else {
+      spinnerColor = Theme.of(context).accentColor;
+    }
     TargetPlatform platform = Theme.of(context).platform;
     if (platform == TargetPlatform.iOS) {
       return CupertinoActivityIndicator();
