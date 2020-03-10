46aeecf3a43faa56e24e2df509daac674526c6b5
Preet Parekh <preetjdp123@gmail.com>
2020-03-10 11:36:11 +0530

    Added "OweMe" text

--- mobileApp/lib/ui/HomePage/oweMe.dart
+++ mobileApp/lib/ui/HomePage/oweMe.dart
@@ -9,16 +9,34 @@ class OweMe extends StatelessWidget {
       child: Stack(
         alignment: Alignment.bottomCenter,
         children: <Widget>[
+          Positioned(
+              left: 0,
+              top: 0,
+              child: Row(
+                children: <Widget>[
+                  Text(
+                    "Owe Me",
+                    style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromRGBO(78, 80, 88, 1)),
+                  ),
+                  Icon(Icons.keyboard_arrow_right,
+                  color: Color.fromRGBO(78, 80, 88, 1),
+                  )
+                ],
+              )),
           Container(
             height: 100,
-            decoration: BoxDecoration(color: Colors.white,
-            borderRadius: BorderRadius.circular(15),
-             boxShadow: [
-              BoxShadow(
-                  blurRadius: 10,
-                  color: Color.fromRGBO(78, 80, 88, 0.05),
-                  spreadRadius: 0.1)
-            ]),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                      blurRadius: 10,
+                      color: Color.fromRGBO(78, 80, 88, 0.05),
+                      spreadRadius: 0.1)
+                ]),
           )
         ],
       ),
