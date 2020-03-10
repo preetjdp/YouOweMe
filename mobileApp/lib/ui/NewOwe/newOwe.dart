2d0ab9f4b1383ce53490eebb2f7a0d155b851e24
Preet Parekh <preetjdp123@gmail.com>
2020-03-11 02:06:13 +0530

    Implement a NewOwePage

--- mobileApp/lib/ui/NewOwe/newOwe.dart
+++ mobileApp/lib/ui/NewOwe/newOwe.dart
@@ -1,3 +1,5 @@
+import 'package:YouOweMe/ui/HomePage/bottomList.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'package:YouOweMe/ui/Abstractions/yomSpinner.dart';
@@ -8,13 +10,75 @@ class NewOwe extends StatelessWidget {
     void onButtonPressed() {
       Navigator.pop(context);
     }
+
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.check),
         onPressed: onButtonPressed,
       ),
-      body: Center(
-        child: YOMSpinner()
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(),
+              Text(
+                "Title",
+                style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromRGBO(78, 80, 88, 1)),
+              ),
+              TextField(
+                cursorColor: Theme.of(context).accentColor,
+                decoration: InputDecoration.collapsed(
+                  hintText: "Enter the reasoning behind this transaction",
+                ),
+                maxLines: 2,
+              ),
+              Text(
+                "Recent People",
+                style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromRGBO(78, 80, 88, 1)),
+              ),
+              BottomList(),
+              Text(
+                "Amount",
+                style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromRGBO(78, 80, 88, 1)),
+              ),
+              Center(
+                child: TextField(
+                  cursorColor: Theme.of(context).accentColor,
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                    hintText: "00",
+                    border: InputBorder.none,
+                    prefixText: "₹",
+                  ),
+                  style: TextStyle(fontSize: 100, fontWeight: FontWeight.w800),
+                ),
+              ),
+              Container(
+                height: 60,
+                width: 400,
+                child: CupertinoButton.filled(
+                    child: Text('Done'), onPressed: () {}),
+              )
+              // Center(
+              //   child: Text(
+              //     "₹ 89",
+              //     style: TextStyle(fontSize: 100, fontWeight: FontWeight.w800),
+              //   ),
+              // )
+            ],
+          ),
+        ),
       ),
     );
   }
