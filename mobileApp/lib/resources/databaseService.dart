9a0ca342d5a309185d54a9a0fc037b1e3b6f6a4c
Preet Parekh <preetjdp123@gmail.com>
2020-03-12 19:41:29 +0530

    Init DatabaseService and addOweToHive function

---
+++ mobileApp/lib/resources/databaseService.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/widgets.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+import 'package:YouOweMe/resources/models/owe.dart';
+
+class DatabaseService {
+  void addOweToHive({@required Owe owe}) {
+    Box<Owe> oweBox = Hive.box("oweBox");
+    if (oweBox.isOpen) {
+      oweBox.add(owe);
+    } else {
+      print("Box is not open");
+    }
+  }
+}
