6117988aedcf4caf04c82ff826909fa1ad3a16d9
Preet Parekh <preetjdp123@gmail.com>
2020-03-12 19:29:11 +0530

    Configure Hive Helper Function

--- mobileApp/lib/main.dart
+++ mobileApp/lib/main.dart
@@ -4,7 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:YouOweMe/resources/helpers.dart';
 import 'package:YouOweMe/ui/HomePage/homePage.dart';
 
-void main() {
+void main() async {
+  await configureHive();
   runApp(MyApp());
 }
 
@@ -20,9 +21,8 @@ class MyApp extends StatelessWidget {
           scaffoldBackgroundColor: Color.fromRGBO(241, 245, 249, 1),
           //This Color is used to set GlowingOverscroll Indicator
           accentColor: Color.fromRGBO(52, 59, 70, 1),
-          cupertinoOverrideTheme: CupertinoThemeData(
-            primaryColor: Color.fromRGBO(52, 59, 70, 1)
-          ),
+          cupertinoOverrideTheme:
+              CupertinoThemeData(primaryColor: Color.fromRGBO(52, 59, 70, 1)),
           floatingActionButtonTheme: FloatingActionButtonThemeData(
             backgroundColor: Color.fromRGBO(52, 59, 70, 1),
             elevation: 2,
