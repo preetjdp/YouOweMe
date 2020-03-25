6dea752f0be671ae69f19e615d48571316bf11fa
Preet Parekh <preetjdp123@gmail.com>
2020-03-25 20:50:30 +0530

    Added AuthChecker and app context

---
+++ server/src/utils/authChecker.ts
@@ -0,0 +1,14 @@
+import { AuthChecker, Root } from "type-graphql";
+import { ApplicationContext } from "./appContext.ts";
+
+export const customAuthChecker: AuthChecker<ApplicationContext> = (
+  { root, args, context, info },
+  roles,
+) => {
+  if (!context.req.headers.authorization) {
+    return false
+  }
+  console.log(context.req.headers.authorization)
+
+  return true;
+};
