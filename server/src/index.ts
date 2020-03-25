7a8a5761d6cc6e3d2179cb753ed547b6ab9e3a6e
Preet Parekh <preetjdp123@gmail.com>
2020-03-26 01:33:09 +0530

    Environment Config

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import { ApolloServer } from "apollo-server-express"
+import "./utils/envConfig"
 
 
 import { generateSchema } from "./schema"
---
+++ server/src/utils/envConfig.ts
@@ -0,0 +1,16 @@
+import { config } from "dotenv"
+
+if (process.env.NODE_ENV == 'development') {
+    config()
+}
+
+
+const PROJECT_ID = process.env.PROJECT_ID as string
+const PRIVATE_KEY = process.env.PRIVATE_KEY as string
+const CLIENT_EMAIL = process.env.CLIENT_EMAIL as string
+
+export {
+    PROJECT_ID,
+    PRIVATE_KEY,
+    CLIENT_EMAIL
+}
