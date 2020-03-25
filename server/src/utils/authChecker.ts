9064d197fdede0ae63e69d9a24699ba393996e0e
Preet Parekh <preetjdp123@gmail.com>
2020-03-25 21:14:05 +0530

    Comment out unused

--- server/src/utils/authChecker.ts
+++ server/src/utils/authChecker.ts
@@ -1,9 +1,14 @@
-import { AuthChecker, Root } from "type-graphql";
+import { AuthChecker } from "type-graphql";
 import { ApplicationContext } from "./appContext";
 
 export const customAuthChecker: AuthChecker<ApplicationContext> = (
-  { root, args, context, info },
-  roles,
+  {
+    // root,
+    // args,
+    context,
+    // info
+  },
+  // roles,
 ) => {
   if (!context.req.headers.authorization) {
     return false
