d20c35f10ce7c4a790fc295612914eb2ba2d33c1
Preet Parekh <preetjdp123@gmail.com>
2020-03-30 18:19:01 +0530

    Firestore as PubSub Resolver

--- server/src/schema.ts
+++ server/src/schema.ts
@@ -1,41 +1,16 @@
-import { gql } from "apollo-server-express"
-
-import { buildSchema, Resolver, Query, Authorized } from "type-graphql"
+import { buildSchema } from "type-graphql"
 import "reflect-metadata"
 
-import { UserResolver } from "./modules/User/UserResolver"
 import { customAuthChecker } from "./utils/authChecker"
+import { PubSubFire } from "./db/pubSubFire"
 
-// const schema = gql`
-// type User {
-//     id: ID!,
-//     name: String!,
-//     image: String
-// }
-
-// type Query {
-//     hello: String,
-//     users: [User!]!
-//     user(id: ID!): User
-//     add(x: Int, y: Int): Int
-//   }
-// `
-
-@Resolver()
-class HelloResolver {
-
-    @Authorized()
-    @Query(() => String)
-    async hello() {
-        return "Hello World"
-    }
-}
 
 const generateSchema = async () => {
     return await buildSchema({
         resolvers: [__dirname + '/modules/**/*.{ts,js}'],
         authChecker: customAuthChecker,
-        dateScalarMode: "timestamp"
+        dateScalarMode: "timestamp",
+        pubSub: PubSubFire
     })
 }
 
