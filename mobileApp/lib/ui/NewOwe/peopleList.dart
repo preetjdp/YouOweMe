05aa6c04d5008a164ed58543dbef1ca3f1cc184c
Preet Parekh <preetjdp123@gmail.com>
2020-04-03 20:50:53 +0530

    Use Slivers for People List

--- mobileApp/lib/ui/NewOwe/peopleList.dart
+++ mobileApp/lib/ui/NewOwe/peopleList.dart
@@ -1,61 +1,71 @@
+import 'package:YouOweMe/ui/Abstractions/expandingWidgetDelegate.dart';
 import 'package:flutter/material.dart';
 
 class PeopleList extends StatelessWidget {
-  final List<String> people = ["Preet Parekh", "Tanay Modi","Elizabeth Cooper"];
+  final List<String> people = [
+    "Preet Parekh",
+    "Tanay Modi",
+    "Elizabeth Cooper"
+  ];
   final EdgeInsets margin = EdgeInsets.all(10);
   @override
   Widget build(BuildContext context) {
     return Container(
-        height: 200,
-        child: ListView(
-          scrollDirection: Axis.horizontal,
-          children: <Widget>[
-            Container(
-              margin: margin.copyWith(left: 0),
-              // height: 100,
-              width: 80,
-              decoration: BoxDecoration(
-                  color: Theme.of(context).accentColor,
-                  borderRadius: BorderRadius.circular(10),
-                  boxShadow: [
-                    BoxShadow(
-                        blurRadius: 10,
-                        color: Color.fromRGBO(78, 80, 88, 0.05),
-                        spreadRadius: 0.1)
-                  ]),
-              child: Center(
-                  child: Icon(
-                Icons.add,
-                size: 38,
-                color: Colors.white,
-              )),
-            ),
-            ...people
-                .map((e) => Container(
-                      margin: margin.copyWith(left: 0),
-                      padding: EdgeInsets.all(5),
-                      // height: 100,
-                      width: 120,
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(10),
-                          boxShadow: [
-                            BoxShadow(
-                                blurRadius: 10,
-                                color: Color.fromRGBO(78, 80, 88, 0.05),
-                                spreadRadius: 0.1)
-                          ]),
-                      child: Center(
-                        child: Text(
-                          e,
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              fontSize: 18, fontWeight: FontWeight.w600),
+      height: 200,
+      child: CustomScrollView(
+        scrollDirection: Axis.horizontal,
+        slivers: [
+          SliverPersistentHeader(
+              pinned: true,
+              delegate: ExpandingWidgetDelegate(
+                  maxWidth: 120,
+                  minWidth: 80,
+                  child: Container(
+                    margin: margin.copyWith(left: 0),
+                    width: 80,
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).accentColor,
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                              blurRadius: 10,
+                              color: Color.fromRGBO(78, 80, 88, 0.05),
+                              spreadRadius: 0.1)
+                        ]),
+                    child: Center(
+                        child: Icon(
+                      Icons.add,
+                      size: 38,
+                      color: Colors.white,
+                    )),
+                  ))),
+          SliverList(
+              delegate: SliverChildBuilderDelegate(
+                  (BuildContext context, int index) => Container(
+                        margin: margin.copyWith(left: 0),
+                        padding: EdgeInsets.all(5),
+                        width: 120,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                            boxShadow: [
+                              BoxShadow(
+                                  blurRadius: 10,
+                                  color: Color.fromRGBO(78, 80, 88, 0.05),
+                                  spreadRadius: 0.1)
+                            ]),
+                        child: Center(
+                          child: Text(
+                            people.elementAt(index),
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.w600),
+                          ),
                         ),
                       ),
-                    ))
-                .toList()
-          ],
-        ));
+                  childCount: people.length))
+        ],
+      ),
+    );
   }
 }
