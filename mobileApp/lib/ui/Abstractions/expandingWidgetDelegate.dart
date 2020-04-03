229161b49d450a116f27a705d2a6d7cdff63b951
Preet Parekh <preetjdp123@gmail.com>
2020-04-03 20:50:29 +0530

    Expanding Widget Delegate

---
+++ mobileApp/lib/ui/Abstractions/expandingWidgetDelegate.dart
@@ -0,0 +1,31 @@
+
+import 'dart:math' as math;
+
+import 'package:flutter/material.dart';
+
+class ExpandingWidgetDelegate extends SliverPersistentHeaderDelegate {
+  ExpandingWidgetDelegate({
+    @required this.minWidth,
+    @required this.maxWidth,
+    @required this.child,
+  });
+  final double minWidth;
+  final double maxWidth;
+  final Widget child;
+  @override
+  double get minExtent => minWidth;
+  @override
+  double get maxExtent => math.max(maxWidth, minWidth);
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return new SizedBox.expand(child: child);
+  }
+
+  @override
+  bool shouldRebuild(ExpandingWidgetDelegate oldDelegate) {
+    return maxWidth != oldDelegate.maxWidth ||
+        minWidth != oldDelegate.minWidth ||
+        child != oldDelegate.child;
+  }
+}
