43da83506b20db44ee02d9300caa0ae209c3ab3b
Preet Parekh <preetjdp123@gmail.com>
2020-04-04 18:37:38 +0530

    Updated Me Query and beta version of artemis

--- mobileApp/lib/resources/graphql/seva.graphql.dart
+++ mobileApp/lib/resources/graphql/seva.graphql.dart
@@ -8,11 +8,87 @@ import 'package:gql/ast.dart';
 part 'seva.graphql.g.dart';
 
 @JsonSerializable(explicitToJson: true)
-class Seva$Query$User with EquatableMixin {
-  Seva$Query$User();
+class Seva$Query$Me$OweMe$IssuedTo with EquatableMixin {
+  Seva$Query$Me$OweMe$IssuedTo();
 
-  factory Seva$Query$User.fromJson(Map<String, dynamic> json) =>
-      _$Seva$Query$UserFromJson(json);
+  factory Seva$Query$Me$OweMe$IssuedTo.fromJson(Map<String, dynamic> json) =>
+      _$Seva$Query$Me$OweMe$IssuedToFromJson(json);
+
+  String id;
+
+  String name;
+
+  String image;
+
+  @override
+  List<Object> get props => [id, name, image];
+  Map<String, dynamic> toJson() => _$Seva$Query$Me$OweMe$IssuedToToJson(this);
+}
+
+@JsonSerializable(explicitToJson: true)
+class Seva$Query$Me$OweMe with EquatableMixin {
+  Seva$Query$Me$OweMe();
+
+  factory Seva$Query$Me$OweMe.fromJson(Map<String, dynamic> json) =>
+      _$Seva$Query$Me$OweMeFromJson(json);
+
+  String id;
+
+  String title;
+
+  double amount;
+
+  Seva$Query$Me$OweMe$IssuedTo issuedTo;
+
+  @override
+  List<Object> get props => [id, title, amount, issuedTo];
+  Map<String, dynamic> toJson() => _$Seva$Query$Me$OweMeToJson(this);
+}
+
+@JsonSerializable(explicitToJson: true)
+class Seva$Query$Me$IOwe$IssuedTo with EquatableMixin {
+  Seva$Query$Me$IOwe$IssuedTo();
+
+  factory Seva$Query$Me$IOwe$IssuedTo.fromJson(Map<String, dynamic> json) =>
+      _$Seva$Query$Me$IOwe$IssuedToFromJson(json);
+
+  String id;
+
+  String name;
+
+  String image;
+
+  @override
+  List<Object> get props => [id, name, image];
+  Map<String, dynamic> toJson() => _$Seva$Query$Me$IOwe$IssuedToToJson(this);
+}
+
+@JsonSerializable(explicitToJson: true)
+class Seva$Query$Me$IOwe with EquatableMixin {
+  Seva$Query$Me$IOwe();
+
+  factory Seva$Query$Me$IOwe.fromJson(Map<String, dynamic> json) =>
+      _$Seva$Query$Me$IOweFromJson(json);
+
+  String id;
+
+  String title;
+
+  double amount;
+
+  Seva$Query$Me$IOwe$IssuedTo issuedTo;
+
+  @override
+  List<Object> get props => [id, title, amount, issuedTo];
+  Map<String, dynamic> toJson() => _$Seva$Query$Me$IOweToJson(this);
+}
+
+@JsonSerializable(explicitToJson: true)
+class Seva$Query$Me with EquatableMixin {
+  Seva$Query$Me();
+
+  factory Seva$Query$Me.fromJson(Map<String, dynamic> json) =>
+      _$Seva$Query$MeFromJson(json);
 
   String id;
 
@@ -24,9 +100,14 @@ class Seva$Query$User with EquatableMixin {
 
   int iOweAmount;
 
+  List<Seva$Query$Me$OweMe> oweMe;
+
+  List<Seva$Query$Me$IOwe> iOwe;
+
   @override
-  List<Object> get props => [id, name, image, oweMeAmount, iOweAmount];
-  Map<String, dynamic> toJson() => _$Seva$Query$UserToJson(this);
+  List<Object> get props =>
+      [id, name, image, oweMeAmount, iOweAmount, oweMe, iOwe];
+  Map<String, dynamic> toJson() => _$Seva$Query$MeToJson(this);
 }
 
 @JsonSerializable(explicitToJson: true)
@@ -36,7 +117,7 @@ class Seva$Query with EquatableMixin {
   factory Seva$Query.fromJson(Map<String, dynamic> json) =>
       _$Seva$QueryFromJson(json);
 
-  Seva$Query$User Me;
+  Seva$Query$Me Me;
 
   @override
   List<Object> get props => [Me];
@@ -49,7 +130,7 @@ class SevaQuery extends GraphQLQuery<Seva$Query, JsonSerializable> {
   @override
   final DocumentNode document = DocumentNode(definitions: [
     OperationDefinitionNode(
-        type: OperationType.subscription,
+        type: OperationType.query,
         name: null,
         variableDefinitions: [],
         directives: [],
@@ -89,7 +170,107 @@ class SevaQuery extends GraphQLQuery<Seva$Query, JsonSerializable> {
                     alias: null,
                     arguments: [],
                     directives: [],
-                    selectionSet: null)
+                    selectionSet: null),
+                FieldNode(
+                    name: NameNode(value: 'oweMe'),
+                    alias: null,
+                    arguments: [],
+                    directives: [],
+                    selectionSet: SelectionSetNode(selections: [
+                      FieldNode(
+                          name: NameNode(value: 'id'),
+                          alias: null,
+                          arguments: [],
+                          directives: [],
+                          selectionSet: null),
+                      FieldNode(
+                          name: NameNode(value: 'title'),
+                          alias: null,
+                          arguments: [],
+                          directives: [],
+                          selectionSet: null),
+                      FieldNode(
+                          name: NameNode(value: 'amount'),
+                          alias: null,
+                          arguments: [],
+                          directives: [],
+                          selectionSet: null),
+                      FieldNode(
+                          name: NameNode(value: 'issuedTo'),
+                          alias: null,
+                          arguments: [],
+                          directives: [],
+                          selectionSet: SelectionSetNode(selections: [
+                            FieldNode(
+                                name: NameNode(value: 'id'),
+                                alias: null,
+                                arguments: [],
+                                directives: [],
+                                selectionSet: null),
+                            FieldNode(
+                                name: NameNode(value: 'name'),
+                                alias: null,
+                                arguments: [],
+                                directives: [],
+                                selectionSet: null),
+                            FieldNode(
+                                name: NameNode(value: 'image'),
+                                alias: null,
+                                arguments: [],
+                                directives: [],
+                                selectionSet: null)
+                          ]))
+                    ])),
+                FieldNode(
+                    name: NameNode(value: 'iOwe'),
+                    alias: null,
+                    arguments: [],
+                    directives: [],
+                    selectionSet: SelectionSetNode(selections: [
+                      FieldNode(
+                          name: NameNode(value: 'id'),
+                          alias: null,
+                          arguments: [],
+                          directives: [],
+                          selectionSet: null),
+                      FieldNode(
+                          name: NameNode(value: 'title'),
+                          alias: null,
+                          arguments: [],
+                          directives: [],
+                          selectionSet: null),
+                      FieldNode(
+                          name: NameNode(value: 'amount'),
+                          alias: null,
+                          arguments: [],
+                          directives: [],
+                          selectionSet: null),
+                      FieldNode(
+                          name: NameNode(value: 'issuedTo'),
+                          alias: null,
+                          arguments: [],
+                          directives: [],
+                          selectionSet: SelectionSetNode(selections: [
+                            FieldNode(
+                                name: NameNode(value: 'id'),
+                                alias: null,
+                                arguments: [],
+                                directives: [],
+                                selectionSet: null),
+                            FieldNode(
+                                name: NameNode(value: 'name'),
+                                alias: null,
+                                arguments: [],
+                                directives: [],
+                                selectionSet: null),
+                            FieldNode(
+                                name: NameNode(value: 'image'),
+                                alias: null,
+                                arguments: [],
+                                directives: [],
+                                selectionSet: null)
+                          ]))
+                    ]))
               ]))
         ]))
   ]);
--- mobileApp/lib/resources/graphql/seva.graphql.g.dart
+++ mobileApp/lib/resources/graphql/seva.graphql.g.dart
@@ -6,29 +6,112 @@ part of 'seva.graphql.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-Seva$Query$User _$Seva$Query$UserFromJson(Map<String, dynamic> json) {
-  return Seva$Query$User()
+Seva$Query$Me$OweMe$IssuedTo _$Seva$Query$Me$OweMe$IssuedToFromJson(
+    Map<String, dynamic> json) {
+  return Seva$Query$Me$OweMe$IssuedTo()
+    ..id = json['id'] as String
+    ..name = json['name'] as String
+    ..image = json['image'] as String;
+}
+
+Map<String, dynamic> _$Seva$Query$Me$OweMe$IssuedToToJson(
+        Seva$Query$Me$OweMe$IssuedTo instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+      'image': instance.image,
+    };
+
+Seva$Query$Me$OweMe _$Seva$Query$Me$OweMeFromJson(Map<String, dynamic> json) {
+  return Seva$Query$Me$OweMe()
+    ..id = json['id'] as String
+    ..title = json['title'] as String
+    ..amount = (json['amount'] as num)?.toDouble()
+    ..issuedTo = json['issuedTo'] == null
+        ? null
+        : Seva$Query$Me$OweMe$IssuedTo.fromJson(
+            json['issuedTo'] as Map<String, dynamic>);
+}
+
+Map<String, dynamic> _$Seva$Query$Me$OweMeToJson(
+        Seva$Query$Me$OweMe instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'title': instance.title,
+      'amount': instance.amount,
+      'issuedTo': instance.issuedTo?.toJson(),
+    };
+
+Seva$Query$Me$IOwe$IssuedTo _$Seva$Query$Me$IOwe$IssuedToFromJson(
+    Map<String, dynamic> json) {
+  return Seva$Query$Me$IOwe$IssuedTo()
+    ..id = json['id'] as String
+    ..name = json['name'] as String
+    ..image = json['image'] as String;
+}
+
+Map<String, dynamic> _$Seva$Query$Me$IOwe$IssuedToToJson(
+        Seva$Query$Me$IOwe$IssuedTo instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+      'image': instance.image,
+    };
+
+Seva$Query$Me$IOwe _$Seva$Query$Me$IOweFromJson(Map<String, dynamic> json) {
+  return Seva$Query$Me$IOwe()
+    ..id = json['id'] as String
+    ..title = json['title'] as String
+    ..amount = (json['amount'] as num)?.toDouble()
+    ..issuedTo = json['issuedTo'] == null
+        ? null
+        : Seva$Query$Me$IOwe$IssuedTo.fromJson(
+            json['issuedTo'] as Map<String, dynamic>);
+}
+
+Map<String, dynamic> _$Seva$Query$Me$IOweToJson(Seva$Query$Me$IOwe instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'title': instance.title,
+      'amount': instance.amount,
+      'issuedTo': instance.issuedTo?.toJson(),
+    };
+
+Seva$Query$Me _$Seva$Query$MeFromJson(Map<String, dynamic> json) {
+  return Seva$Query$Me()
     ..id = json['id'] as String
     ..name = json['name'] as String
     ..image = json['image'] as String
     ..oweMeAmount = json['oweMeAmount'] as int
-    ..iOweAmount = json['iOweAmount'] as int;
+    ..iOweAmount = json['iOweAmount'] as int
+    ..oweMe = (json['oweMe'] as List)
+        ?.map((e) => e == null
+            ? null
+            : Seva$Query$Me$OweMe.fromJson(e as Map<String, dynamic>))
+        ?.toList()
+    ..iOwe = (json['iOwe'] as List)
+        ?.map((e) => e == null
+            ? null
+            : Seva$Query$Me$IOwe.fromJson(e as Map<String, dynamic>))
+        ?.toList();
 }
 
-Map<String, dynamic> _$Seva$Query$UserToJson(Seva$Query$User instance) =>
+Map<String, dynamic> _$Seva$Query$MeToJson(Seva$Query$Me instance) =>
     <String, dynamic>{
       'id': instance.id,
       'name': instance.name,
       'image': instance.image,
       'oweMeAmount': instance.oweMeAmount,
       'iOweAmount': instance.iOweAmount,
+      'oweMe': instance.oweMe?.map((e) => e?.toJson())?.toList(),
+      'iOwe': instance.iOwe?.map((e) => e?.toJson())?.toList(),
     };
 
 Seva$Query _$Seva$QueryFromJson(Map<String, dynamic> json) {
   return Seva$Query()
     ..Me = json['Me'] == null
         ? null
-        : Seva$Query$User.fromJson(json['Me'] as Map<String, dynamic>);
+        : Seva$Query$Me.fromJson(json['Me'] as Map<String, dynamic>);
 }
 
 Map<String, dynamic> _$Seva$QueryToJson(Seva$Query instance) =>
