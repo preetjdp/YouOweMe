275d14c0e2a70f06c65caa7a0f284d26521c425d
Preet Parekh <preetjdp123@gmail.com>
2020-04-11 20:49:04 +0530

    sample mark as paid

--- mobileApp/lib/ui/HomePage/bottomList.dart
+++ mobileApp/lib/ui/HomePage/bottomList.dart
@@ -1,11 +1,49 @@
 import 'package:YouOweMe/resources/graphql/seva.dart';
 import 'package:YouOweMe/resources/notifiers/meNotifier.dart';
 import 'package:YouOweMe/ui/Abstractions/yomAvatar.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class BottomList extends StatelessWidget {
+  void onTick(Seva$Query$User$Owe owe, BuildContext context) async {
+    bool shouldDelete = false;
+    Widget actionSheet = CupertinoActionSheet(
+      message: Text("This action will mark the `owe` as paid."),
+      cancelButton: CupertinoActionSheetAction(
+        child: Text("Cancel"),
+        isDestructiveAction: true,
+        onPressed: () {
+          Navigator.pop(context, false);
+        },
+      ),
+      actions: <Widget>[
+        CupertinoActionSheetAction(
+          child: Text(
+            "Paid",
+            style: TextStyle(color: CupertinoColors.activeGreen),
+          ),
+          isDefaultAction: true,
+          onPressed: () {
+            Navigator.pop(context, true);
+          },
+        ),
+      ],
+    );
+    shouldDelete = await showCupertinoModalPopup(
+        context: context, builder: (BuildContext context) => actionSheet);
+    if (shouldDelete) {
+      DocumentReference oweRef = Firestore.instance
+          .collection("users")
+          .document(owe.issuedBy.id)
+          .collection("owes")
+          .document(owe.id);
+      oweRef.delete();
+      Provider.of<MeNotifier>(context, listen: false).refresh();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
@@ -33,6 +71,14 @@ class BottomList extends StatelessWidget {
                 ),
                 Expanded(
                   child: Container(),
+                ),
+                CupertinoButton(
+                  onPressed: () => onTick(owe, context),
+                  padding: EdgeInsets.symmetric(vertical: 2, horizontal: 10),
+                  child: Icon(
+                    CupertinoIcons.check_mark_circled,
+                    size: 28,
+                  ),
                 ),
                 CupertinoButton(
                     color: Theme.of(context).accentColor,
