e69033408c68d071ea7fb300c65a2e04625f0813
Preet Parekh <preetjdp123@gmail.com>
2020-04-14 00:05:21 +0530

    pass Callbacks and static height in yomAvatar

--- mobileApp/lib/ui/Abstractions/yomAvatar.dart
+++ mobileApp/lib/ui/Abstractions/yomAvatar.dart
@@ -4,34 +4,44 @@ import 'package:flutter/material.dart';
 class YomAvatar extends StatelessWidget {
   final String text;
   final double size;
-  YomAvatar({this.size = 28, this.text = "PP"});
+  VoidCallback onPressed;
+  YomAvatar({this.size = 45, this.text = "PP", this.onPressed});
   @override
   Widget build(BuildContext context) {
     final Color color = Theme.of(context).accentColor;
-    return Container(
-      decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        gradient: LinearGradient(
-          begin: FractionalOffset.topCenter,
-          end: FractionalOffset.bottomCenter,
-          colors: <Color>[
-            color,
-            Color.fromARGB(
-              color.alpha,
-              (color.red + 50).clamp(0, 255) as int,
-              (color.green + 50).clamp(0, 255) as int,
-              (color.blue + 50).clamp(0, 255) as int,
-            ),
-          ],
+    return CupertinoButton(
+      minSize: 0,
+      onPressed: onPressed,
+      padding: EdgeInsets.all(0),
+      child: Container(
+        height: size,
+        width: size,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          gradient: LinearGradient(
+            begin: FractionalOffset.topCenter,
+            end: FractionalOffset.bottomCenter,
+            colors: <Color>[
+              color,
+              Color.fromARGB(
+                color.alpha,
+                (color.red + 50).clamp(0, 255) as int,
+                (color.green + 50).clamp(0, 255) as int,
+                (color.blue + 50).clamp(0, 255) as int,
+              ),
+            ],
+          ),
         ),
-      ),
-      padding: const EdgeInsets.all(12.0),
-      child: Text(
-        text,
-        style: const TextStyle(
-          color: CupertinoColors.white,
-          fontSize: 13.0,
-          fontWeight: FontWeight.w500,
+        padding: const EdgeInsets.all(12.0),
+        child: Center(
+          child: Text(
+            text,
+            style: const TextStyle(
+              color: CupertinoColors.white,
+              fontSize: 13.0,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
         ),
       ),
     );
