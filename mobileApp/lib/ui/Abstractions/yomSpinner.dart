f76f3808526585cacd227c22430927289e3567e4
Preet Parekh <preetjdp123@gmail.com>
2020-04-19 16:23:43 +0530

    updated YomSpinner

--- mobileApp/lib/ui/Abstractions/yomSpinner.dart
+++ mobileApp/lib/ui/Abstractions/yomSpinner.dart
@@ -3,18 +3,22 @@ import 'package:flutter/material.dart';
 
 class YOMSpinner extends StatelessWidget {
   final Brightness brightness;
-  YOMSpinner({this.brightness = Brightness.dark});
+  YOMSpinner({this.brightness = Brightness.light});
   @override
   Widget build(BuildContext context) {
     Color spinnerColor;
-    if(brightness == Brightness.light) {
+    if (brightness == Brightness.dark) {
       spinnerColor = Colors.white;
     } else {
       spinnerColor = Theme.of(context).accentColor;
     }
     TargetPlatform platform = Theme.of(context).platform;
     if (platform == TargetPlatform.iOS) {
-      return CupertinoActivityIndicator();
+      return Theme(
+          data: ThemeData(
+              cupertinoOverrideTheme:
+                  CupertinoThemeData(brightness: brightness)),
+          child: CupertinoActivityIndicator());
     } else {
       return CircularProgressIndicator(
           strokeWidth: 4,
