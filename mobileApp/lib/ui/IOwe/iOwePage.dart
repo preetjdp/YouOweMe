577b54e516fcd69dff78cb645b0303cddda76864
Preet Parekh <preetjdp123@gmail.com>
2020-04-19 19:45:18 +0530

    I Owe Page

--- mobileApp/lib/ui/IOwe/iOwePage.dart
+++ mobileApp/lib/ui/IOwe/iOwePage.dart
@@ -1,10 +1,16 @@
+import 'package:YouOweMe/resources/graphql/seva.dart';
+import 'package:YouOweMe/resources/notifiers/meNotifier.dart';
+import 'package:YouOweMe/ui/Abstractions/yomAvatar.dart';
 import 'package:YouOweMe/ui/NewOwe/newOwe.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class IOwePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final List<Seva$Query$User$Owe> iOwe =
+        Provider.of<MeNotifier>(context).me.iOwe;
     return Scaffold(
         appBar: CupertinoNavigationBar(
           backgroundColor: Theme.of(context).backgroundColor.withOpacity(0.5),
@@ -19,9 +25,59 @@ class IOwePage extends StatelessWidget {
           actionsForegroundColor: Theme.of(context).accentColor,
         ),
         body: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: IOwePageEmptyState(),
-        ));
+            padding: const EdgeInsets.all(15.0),
+            child: iOwe.isNotEmpty
+                ? ListView.builder(
+                    itemCount: iOwe.length,
+                    physics: NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemBuilder: (BuildContext context, int index) {
+                      Seva$Query$User$Owe owe = iOwe[index];
+                      return Container(
+                        margin: EdgeInsets.only(top: 10),
+                        height: 50,
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            YomAvatar(
+                              text: "PP",
+                            ),
+                            SizedBox(
+                              width: 20,
+                            ),
+                            Text(
+                              owe.title,
+                              style: Theme.of(context).textTheme.headline3,
+                            ),
+                            Expanded(
+                              child: Container(),
+                            ),
+                            CupertinoButton(
+                              onPressed: () {},
+                              padding: EdgeInsets.symmetric(
+                                  vertical: 2, horizontal: 10),
+                              child: Icon(
+                                CupertinoIcons.check_mark_circled,
+                                size: 28,
+                              ),
+                            ),
+                            CupertinoButton(
+                                color: Theme.of(context).accentColor,
+                                minSize: 20,
+                                padding: EdgeInsets.all(10),
+                                child: Text(
+                                  owe.amount.toString(),
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .headline3
+                                      .copyWith(color: Colors.white),
+                                ),
+                                onPressed: () {})
+                          ],
+                        ),
+                      );
+                    })
+                : IOwePageEmptyState()));
   }
 }
 
