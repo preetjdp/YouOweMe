1e4796946cf05a551466354fbcdf3e2b39a13e4e
Preet Parekh <preetjdp123@gmail.com>
2020-04-30 23:21:49 +0530

    Api to update user Information

---
+++ server/src/modules/User/updateUserResolver.ts
@@ -0,0 +1,32 @@
+import { Resolver, Arg, Mutation, Authorized } from "type-graphql";
+import { User } from "../../models/User";
+import { UpdateUserInputType } from "./updateUser/updateUserInputType.ts";
+import { firestore } from "../../db/firebase";
+import { UserResolver } from "./UserResolver"
+
+@Resolver(User)
+export class UpdateUserResolver {
+    @Authorized()
+    @Mutation(() => User, {
+        description: "This Mutation gives one the ability to update values of a `User`. Currently only supports updating the name and the fcm_token",
+    })
+    async updateUser(@Arg("data") data: UpdateUserInputType) {
+        const userId = data.id
+        const userRef = firestore.collection('users').doc(userId)
+        try {
+            // This is a anti-pattern
+            // Would not recommend for larger systems
+            // Is done bacause fieldNames are different in server and database
+            let { id, fcmToken, ...updateData }: any = data
+            console.log(updateData)
+            await userRef.update({
+                ...(data.fcmToken && { fcm_token: data.fcmToken }),
+                ...updateData
+            })
+            let user = await new UserResolver().getUser(userId)
+            return user
+        } catch (e) {
+            throw e
+        }
+    }
+}
