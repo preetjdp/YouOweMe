f43b291798c25626a577f58db86e83b275872034
Preet Parekh <preetjdp123@gmail.com>
2020-05-01 16:26:20 +0530

    Send Notification If fcmToken is present

--- firebase/functions/src/db/firebase.ts
+++ firebase/functions/src/db/firebase.ts
@@ -4,8 +4,25 @@ admin.initializeApp()
 
 const firestore = admin.firestore()
 const auth = admin.auth()
+const fcm = admin.messaging()
+
+interface sendFcmNotificationInterface {
+    deviceToken: string
+    title: string
+    body: string
+}
+
+const sendFcmNotification = ({ deviceToken, title, body }: sendFcmNotificationInterface) => {
+    return fcm.sendToDevice(deviceToken, {
+        notification: {
+            title: title,
+            body: body
+        }
+    })
+}
 
 export {
     firestore,
-    auth
+    auth,
+    sendFcmNotification
 }
