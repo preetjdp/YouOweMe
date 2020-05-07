e44900163b7aedae2c75a228fab36685c892a494
Preet Parekh <preetjdp123@gmail.com>
2020-05-08 01:39:17 +0530

    Implemented a super Simple YomButton

---
+++ mobileApp/lib/ui/Abstractions/yomButton.dart
@@ -0,0 +1,98 @@
+import 'package:YouOweMe/ui/Abstractions/yomSpinner.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:basics/basics.dart';
+
+class YomButton extends StatelessWidget {
+  final YomButtonController controller;
+
+  final Widget child;
+
+  final Widget loading;
+
+  final Widget error;
+
+  final Widget success;
+
+  final VoidCallback onPressed;
+
+  final Color backgroundColor;
+
+  YomButton({
+    @required this.controller,
+    @required this.child,
+    @required this.onPressed,
+    this.loading,
+    this.error,
+    this.success,
+    this.backgroundColor,
+  }) : assert(controller != null);
+
+  @override
+  Widget build(BuildContext context) {
+    Widget _buttonChild;
+    Color _buttonBackgroundColor =
+        this.backgroundColor ?? Theme.of(context).accentColor;
+    Brightness _buttonBrightness =
+        ThemeData.estimateBrightnessForColor(_buttonBackgroundColor);
+
+    Color _iconsColor;
+    if (_buttonBrightness == Brightness.dark) {
+      _iconsColor = Theme.of(context).scaffoldBackgroundColor;
+    } else {
+      _iconsColor = Theme.of(context).accentColor;
+    }
+    return StreamBuilder<YomButtonState>(
+        stream: controller.state,
+        builder: (BuildContext context, snapshot) {
+          YomButtonState buttonState = snapshot?.data ?? YomButtonState.ACTIVE;
+          if (buttonState == YomButtonState.ACTIVE) {
+            _buttonChild = child;
+          } else if (buttonState == YomButtonState.LOADING) {
+            _buttonChild = this.loading ??
+                YOMSpinner(
+                  brightness: _buttonBrightness,
+                );
+          } else if (buttonState == YomButtonState.ERROR) {
+            _buttonChild = this.error ??
+                Icon(
+                  Icons.error_outline,
+                  color: _iconsColor,
+                );
+          } else if (buttonState == YomButtonState.SUCCESS) {
+            _buttonChild = this.success ??
+                Icon(
+                  Icons.check_circle_outline,
+                  color: _iconsColor,
+                );
+          }
+
+          return CupertinoButton(
+            color: _buttonBackgroundColor,
+            child: AnimatedSwitcher(
+                duration: Duration(milliseconds: 200), child: _buttonChild),
+            onPressed: onPressed,
+          );
+        });
+  }
+}
+
+enum YomButtonState {
+  ACTIVE,
+//  INACTIVE,
+  LOADING,
+  ERROR,
+  SUCCESS
+}
+
+class YomButtonController {
+  BehaviorSubject<YomButtonState> _yomButtonSubject =
+      BehaviorSubject.seeded(YomButtonState.ACTIVE);
+
+  Stream<YomButtonState> get state => _yomButtonSubject.stream;
+
+  void setButtonState(YomButtonState state) {
+    _yomButtonSubject.add(state);
+  }
+}
