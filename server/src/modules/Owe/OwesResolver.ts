a0fc820871fa9a668bb3f32a04cfdedee0f0d6d7
Preet Parekh <preetjdp123@gmail.com>
2020-05-10 18:15:30 +0530

    Permalink Logic in OwesResolver

--- server/src/modules/Owe/OwesResolver.ts
+++ server/src/modules/Owe/OwesResolver.ts
@@ -2,6 +2,7 @@ import { Resolver, Query } from "type-graphql";
 import { Owe, OweState } from "../../models/Owe";
 import { firestore } from "../../db/firebase";
 import { Timestamp, DocumentReference } from "@google-cloud/firestore"
+import { getPermalinkFromOwe } from "../../utils/helpers";
 
 @Resolver()
 export class OwesResolver {
@@ -9,10 +10,11 @@ export class OwesResolver {
     @Query(() => [Owe])
     async getOwes() {
         const owesSnapshot = await firestore.collectionGroup('owes').get()
-        const owes: Array<Owe> = owesSnapshot.docs.map((oweF) => {
+        const owes: Array<Owe> = await Promise.all(owesSnapshot.docs.map(async (oweF) => {
             const oweFData = oweF.data()
             const oweFCreated: Timestamp = oweFData.created
             const issedToRef: DocumentReference = oweFData.issuedToRef
+            const permalink = await getPermalinkFromOwe(oweF)
             const owe: Owe = {
                 id: oweF.id,
                 documenmentRef: oweF.ref,
@@ -21,10 +23,11 @@ export class OwesResolver {
                 state: oweFData.state ?? OweState.CREATED,
                 issuedByID: oweF.ref.parent.parent!.id,
                 issuedToID: issedToRef.id,
-                created: oweFCreated.toDate()
+                created: oweFCreated.toDate(),
+                permalink: permalink
             }
             return owe
-        })
+        }))
         return owes
     }
 }
