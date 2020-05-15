a58933e7c2aac26e12c7dcaae3aa2c0a19c18597
Preet Parekh <preetjdp123@gmail.com>
2020-05-15 19:35:59 +0530

    Platform Level Code for Notifications

---
+++ mobileApp/android/app/src/main/kotlin/dev/preetjdp/youoweme/FCMApplication.kt
@@ -0,0 +1,26 @@
+package dev.preetjdp.youoweme
+
+import io.flutter.app.FlutterApplication
+import io.flutter.plugin.common.PluginRegistry
+import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback
+import io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin
+import io.flutter.view.FlutterMain
+import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService
+import com.dexterous.flutterlocalnotifications.FlutterLocalNotificationsPlugin
+
+class FCMApplication : FlutterApplication(), PluginRegistrantCallback {
+
+    override fun onCreate() {
+        super.onCreate()
+        FlutterFirebaseMessagingService.setPluginRegistrant(this);
+        FlutterMain.startInitialization(this)
+    }
+
+    override fun registerWith(registry: PluginRegistry?) {
+        if (!registry!!.hasPlugin("io.flutter.plugins.firebasemessaging")) {
+            FirebaseMessagingPlugin.registerWith(registry!!.registrarFor("io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin"));
+            FlutterLocalNotificationsPlugin.registerWith(registry.registrarFor("com.dexterous.flutterlocalnotifications.FlutterLocalNotificationsPlugin"));
+        }
+    }
+}
+
