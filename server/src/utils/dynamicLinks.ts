87d9c5408635101712f6d78b594b2261e0dadc6c
Preet Parekh <preetjdp123@gmail.com>
2020-05-26 03:08:34 +0530

    Generate DynamicLinks using REST calls.

--- server/src/utils/dynamicLinks.ts
+++ server/src/utils/dynamicLinks.ts
@@ -1,57 +1,59 @@
 import axios from "axios"
-import buildUrl from "build-url"
 
-export class DynamicLinks {
-    dynamicLinkParameters: DynamicLinkParameters
-    constructor(parameters: DynamicLinkParameters) {
-        this.dynamicLinkParameters = parameters
-    }
-
-    buildShortLink() {
-        axios.post("https://Google.com",{
-            data: {}
-        })
-    }
-
-    // async buildShortLink(): Promise<string> {
-    // }
-}
-
-interface DynamicLinkParameters {
-    androidParameters?: AndroidParameters
-
-    urlPrefix: string
-
-    iosParameters?: IosParameters
-
+/**
+ * @params link The link your app will open.
+ * @params domainUriPrefix The URL Prefix for the app.
+ * @params androidParameters The Android Parameters for the URL.
+ * @params iosParameters The IOS Parameters for the URL.
+ * @params navigationInfoParameters The Navigation Parameters for the URL.
+ * @params suffix Should the end url be short or long
+ */
+type DynamicLinkParameters = {
     link: string
-
-    navigationInfoParameters?: NavigationInfoParameters
-
+    domainUriPrefix: string
+    androidInfo: AndroidParameters
+    iosInfo: IosParameters
+    socialMetaTagInfo: SocialMetaTagParameters
+    navigationInfo?: NavigationInfoParameters
 }
 
-interface AndroidParameters {
-    packageName: string
-    fallbackUrl?: string
-    minimumVersion?: number
+type AndroidParameters = {
+    /** The package name of the Android app to use to open the link. */
+    androidPackageName: string
+    /** The link to open when the app isn't installed.  */
+    androidFallbackLink?: string
 }
 
-interface IosParameters {
-    appStoreId: string
-
-    bundleId: string
-
-    customScheme?: string
-
-    fallbackUrl?: string
-
-    ipadBundleId?: string
-
-    ipadFallbackUrl?: string
-
-    minimumVersion?: string
+type IosParameters = {
+    /** The bundle ID of the iOS app to use to open the link. */
+    iosBundleId: string
 }
 
-interface NavigationInfoParameters {
-    forcedRedirectEnabled: boolean
+type NavigationInfoParameters = {
+    /** If set to '1', skip the app preview page when the Dynamic Link is opened, and instead redirect to the app or store. */
+    enableForcedRedirect: boolean
+}
+
+type SocialMetaTagParameters = {
+    /** The title to use when the Dynamic Link is shared in a social post. */
+    socialTitle: string
+    /** The description to use when the Dynamic Link is shared in a social post. */
+    socialDescription?: string
+    /** The URL to an image related to this link. The image should be at least 300x200 px, and less than 300 KB.*/
+    socialImageLink?: string
+}
+
+type SuffixParameter = {
+    option: "SHORT" | "UNGUESSABLE"
+}
+
+
+export const generateDynamicLink = async (parameters: DynamicLinkParameters, suffix: SuffixParameter): Promise<string> => {
+    const api_key = "AIzaSyC9mDgysM-vQlguS3X52Kx42sOmrDtKVAY"
+    const requestBody = {
+        dynamicLinkInfo: parameters,
+        suffix
+    }
+    const response = await axios.post(`https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=${api_key}`, requestBody)
+    return response.data.shortLink
 }
--- server/src/utils/helpers.ts
+++ server/src/utils/helpers.ts
@@ -1,17 +1,36 @@
 import { DocumentSnapshot } from "@google-cloud/firestore"
-import { DynamicLinks } from "../utils/dynamicLinks"
+import { generateDynamicLink } from "../utils/dynamicLinks"
 
+/**
+ * Steps to be followed here.
+ * @params DocumentSnapshot for the owe.
+ * @returns The permalink for the `Owe`
+ * 1. Check if Permalink exists in the document, if Yes, Return it.
+ * 2. If No Generate the Url.
+ * 3. Set that Url in Firestore and return the Url.
+ */
 export const getPermalinkFromOwe = async (snapshot: DocumentSnapshot): Promise<string> => {
     const oweData = snapshot.data()!
+    const oweId = snapshot.id
     if (oweData.permalink) {
         return oweData.permalink
     }
-    const parameters = new DynamicLinks({
-        link: "https://google.com",
-        urlPrefix: "wow",
-        androidParameters: {
-            packageName: "wpw"
-        }
+    const link = generateDynamicLink({
+        link: `https://youoweme.preetjdp.dev/owe/${oweId}`,
+        domainUriPrefix: "youoweme.page.link",
+        androidInfo: {
+            androidPackageName: "dev.preetjdp.youoweme"
+        },
+        iosInfo: {
+            iosBundleId: "dev.preetjdp.youoweme",
+        },
+        socialMetaTagInfo: {
+            socialTitle: "New Owe",
+        },
+    }, {
+        option: "SHORT"
     })
-    
+
+    return link
+
 }
