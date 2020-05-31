45c4b3b4e45a4caea1773956914217a7a6f0a4c2
Preet Parekh <preetjdp123@gmail.com>
2020-05-31 10:51:32 +0530

    Removed asyncForEach

--- server/src/utils/asyncForEach.ts
+++
@@ -1,5 +0,0 @@
-export async function asyncForEach<T>(array: T[], callback: (item: T, index: number, allItems: T[]) => void) {
-    for (let index = 0; index < array.length; index++) {
-      await callback(array[index], index, array);
-    }
-  }
