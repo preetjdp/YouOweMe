4ccafe4289890b79978f7410369ffeba43f1beab
Preet Parekh <preetjdp123@gmail.com>
2020-05-31 23:31:14 +0530

    getOweQuery in seva

--- server/src/modules/Owe/OweResolver.ts
+++ server/src/modules/Owe/OweResolver.ts
@@ -1,4 +1,4 @@
-import { Resolver, FieldResolver, Root, } from "type-graphql";
+import { Resolver, FieldResolver, Root, Query, Arg, } from "type-graphql";
 import { Owe, OweState } from "../../models/Owe";
 import { User } from "../../models/User";
 import { UserResolver } from "../User/UserResolver";
@@ -7,6 +7,7 @@ import { getPermalinkFromOwe } from "../../utils/helpers"
 import { RequestContainer, UserDataLoader } from "../User/userResolver/userLoader";
 import { mapUserSnapshot } from "../User/userResolver/userSnapshotMap";
 import { mapOweSnapshot } from "./oweResolver/oweSnapshotMap";
+import { firestore } from "../../db/firebase";
 
 @Resolver(Owe)
 export class OweResolver {
@@ -16,7 +17,18 @@ export class OweResolver {
         return owe
     }
 
-    async getOwesFromUserRef() { }
+    @Query(() => Owe, { name: "getOwe" },)
+    async getOweFromId(@Arg("id") id: string): Promise<Owe> {
+        const owes = await firestore.collectionGroup("owes").get()
+        const filteredOwes = owes.docs.filter(doc => doc.id == id)
+        if (filteredOwes.length == 0) {
+            throw `No Owe with the Document ID ${id} found.`
+        }
+        const oweSnapshot = filteredOwes[0]
+        const owe: Owe = await mapOweSnapshot(oweSnapshot)
+        return owe
+    }
+
     @FieldResolver(() => User, {
         name: "issuedBy",
     })
