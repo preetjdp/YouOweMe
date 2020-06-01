bb3d4e7c04c9ef5f5d25f8d1953e59a75e1ccbf9
Preet Parekh <preetjdp123@gmail.com>
2020-06-01 15:02:04 +0530

    BottomSheet for firebase dynmaic links

--- mobileApp/lib/resources/helpers/firebaseDynamicLinks/bottomSheet.dart
+++ mobileApp/lib/resources/helpers/firebaseDynamicLinks/bottomSheet.dart
@@ -1,11 +1,16 @@
 import 'package:YouOweMe/resources/graphql/queries/getOwe/getOwe.dart';
+import 'package:YouOweMe/resources/graphql/seva.dart' as seva;
 import 'package:YouOweMe/resources/notifiers/meNotifier.dart';
+import 'package:YouOweMe/ui/Abstractions/yomButton.dart';
 import 'package:YouOweMe/ui/Abstractions/yomSpinner.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:provider/provider.dart';
 import 'package:basics/basics.dart';
 import 'package:async/async.dart';
+import 'package:YouOweMe/resources/extensions.dart';
 
 class FirebaseDynamicLinkBottomSheet extends StatefulWidget {
   final ScrollController scrollController;
@@ -35,20 +40,19 @@ class _FirebaseDynamicLinkBottomSheetState
       borderRadius: BorderRadius.only(
           topLeft: Radius.circular(15), topRight: Radius.circular(15)),
       child: Material(
-        child: Container(
-            height: 400,
-            child: FutureBuilder<QueryResult>(
-                future: _queryResultMemoizer
-                    .runOnce(() => graphQLClient.query(queryOptions)),
-                builder: (BuildContext context,
-                    AsyncSnapshot<QueryResult> snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return Center(child: YOMSpinner());
-                  }
-                  GetOwe$Query getOweResult =
-                      GetOwe$Query.fromJson(snapshot.data.data);
-                  return BottomSheetContent(getOweResult.getOwe);
-                })),
+        child: FutureBuilder<QueryResult>(
+            future: _queryResultMemoizer
+                .runOnce(() => graphQLClient.query(queryOptions)),
+            builder:
+                (BuildContext context, AsyncSnapshot<QueryResult> snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Container(
+                    height: 120, child: Center(child: YOMSpinner()));
+              }
+              GetOwe$Query getOweResult =
+                  GetOwe$Query.fromJson(snapshot.data.data);
+              return BottomSheetContent(getOweResult.getOwe);
+            }),
       ),
     );
   }
@@ -59,6 +63,85 @@ class BottomSheetContent extends StatelessWidget {
   BottomSheetContent(this.owe);
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text(owe.title));
+    seva.Seva$Query$User me = context.watch<MeNotifier>().me;
+    return ListView(
+      shrinkWrap: true,
+      padding: EdgeInsets.all(15),
+      children: [
+        Text("Title", style: Theme.of(context).textTheme.headline5),
+        Text(owe.title, style: Theme.of(context).textTheme.bodyText2),
+        SizedBox(
+          height: 20,
+        ),
+        if ([OweState.ACKNOWLEDGED, OweState.CREATED].contains(owe.state))
+          Text("Amount To Be Paid",
+              style: Theme.of(context).textTheme.headline5)
+        else if (owe.state == OweState.PAID)
+          Text("Amount Paid", style: Theme.of(context).textTheme.headline5),
+        RichText(
+          text:
+              TextSpan(style: Theme.of(context).textTheme.headline1, children: [
+            TextSpan(
+                text: "₹",
+                style: TextStyle(color: Theme.of(context).accentColor)),
+            TextSpan(text: owe.amount.toInt().toString())
+          ]),
+        ),
+        Text("Wait When was this Again?",
+            style: Theme.of(context).textTheme.headline5),
+        Text(owe.created.simpler, style: Theme.of(context).textTheme.bodyText2),
+        if (owe.issuedTo.id == me.id &&
+            [OweState.ACKNOWLEDGED, OweState.CREATED].contains(owe.state)) ...[
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            height: 60,
+            width: 400,
+            child: CupertinoButton(
+                color: Theme.of(context).accentColor,
+                child: Text('Pay Up!'),
+                onPressed: () {
+                  showCupertinoModalBottomSheet(
+                      context: context,
+                      builder: (a, b) => Center(child: YOMSpinner()));
+                }),
+          ),
+        ] else if (owe.issuedBy.id == me.id) ...[
+          if ([OweState.ACKNOWLEDGED, OweState.CREATED]
+              .contains(owe.state)) ...[
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 60,
+              width: 400,
+              child: CupertinoButton(
+                  color: Theme.of(context).accentColor,
+                  child: Text('Ping Him Up!'),
+                  onPressed: () {}),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              height: 60,
+              width: 400,
+              child: YomButton(
+                  backgroundColor: CupertinoColors.activeGreen,
+                  child: Text('Mark As Paid'),
+                  onPressed: () {}),
+            ),
+          ],
+          SizedBox(
+            height: 10,
+          ),
+          YomButton(
+            onPressed: () {},
+            child: Text("Delete This Owe  👹", textAlign: TextAlign.center),
+          ),
+        ]
+      ],
+    );
   }
 }
