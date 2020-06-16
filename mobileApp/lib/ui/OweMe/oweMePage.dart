90808bf2860d8c4a25255c743bb3d01b6727c0f4
Preet Parekh <preetjdp123@gmail.com>
2020-06-16 20:28:28 +0530

    Handwritten total (#51)
    * Added Rough
    
    * yomSpacer and total extension
    
    * Show the Total!

--- mobileApp/lib/ui/OweMe/oweMePage.dart
+++ mobileApp/lib/ui/OweMe/oweMePage.dart
@@ -1,4 +1,6 @@
 // 🐦 Flutter imports:
+import 'package:YouOweMe/ui/Abstractions/yomSpacer.dart';
+import 'package:YouOweMe/ui/Abstractions/yomTheme.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -11,12 +13,56 @@ import 'package:YouOweMe/resources/notifiers/meNotifier.dart';
 import 'package:YouOweMe/ui/OweMe/oweMePageElement.dart';
 import 'package:YouOweMe/ui/OweMe/oweMePageEmptyState.dart';
 import 'package:YouOweMe/resources/extensions.dart';
+import 'package:rough/rough.dart';
 
 class OweMePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final List<Seva$Query$User$Owe> oweMe =
         Provider.of<MeNotifier>(context).me.oweMe;
+
+    List<Widget> _getHandDrawnTotal(List<Seva$Query$User$Owe> owes) {
+      return <Widget>[
+        SliverPadding(
+          padding: EdgeInsets.only(left: 15, right: 15, top: 10),
+          sliver: SliverToBoxAdapter(
+            child: CustomPaint(
+              size: Size(0, 10),
+              painter: HandDrawnLinePainter(width: 100),
+            ),
+          ),
+        ),
+        SliverPadding(
+          padding: EdgeInsets.symmetric(horizontal: 15),
+          sliver: SliverToBoxAdapter(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  "Total : ",
+                  style: Theme.of(context).textTheme.headline5,
+                ),
+                Container(
+                  constraints: BoxConstraints(minWidth: 65),
+                  child: CupertinoButton(
+                      color: Theme.of(context).accentColor,
+                      minSize: 20,
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        owes.total.toString(),
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline5
+                            .copyWith(color: Colors.white),
+                      ),
+                      onPressed: () {}),
+                )
+              ],
+            ),
+          ),
+        ),
+      ];
+    }
 
     return Scaffold(
         appBar: CupertinoNavigationBar(
@@ -35,8 +81,11 @@ class OweMePage extends StatelessWidget {
             ? CustomScrollView(
                 slivers: [
                   if (oweMe.stateAcknowledged.length > 0) ...[
+                    SliverYomSpacer(
+                      height: 10,
+                    ),
                     SliverPadding(
-                      padding: EdgeInsets.only(left: 15, top: 15),
+                      padding: EdgeInsets.symmetric(horizontal: 15),
                       sliver: SliverToBoxAdapter(
                         child: Text("Acknowledged",
                             style: Theme.of(context).textTheme.headline5),
@@ -58,9 +107,15 @@ class OweMePage extends StatelessWidget {
                       ),
                     )
                   ],
+                  if (oweMe.stateAcknowledged.length > 1)
+                    ..._getHandDrawnTotal(oweMe.stateAcknowledged)
+                  else
+                    SliverYomSpacer(
+                      height: 10,
+                    ),
                   if (oweMe.stateCreated.length > 0) ...[
                     SliverPadding(
-                      padding: EdgeInsets.only(left: 15, top: 10),
+                      padding: EdgeInsets.symmetric(horizontal: 15),
                       sliver: SliverToBoxAdapter(
                         child: Text("Still Open",
                             style: Theme.of(context).textTheme.headline5),
@@ -81,9 +136,15 @@ class OweMePage extends StatelessWidget {
                       ),
                     )
                   ],
+                  if (oweMe.stateCreated.length > 1)
+                    ..._getHandDrawnTotal(oweMe.stateCreated)
+                  else
+                    SliverYomSpacer(
+                      height: 10,
+                    ),
                   if (oweMe.statePaid.length > 0) ...[
                     SliverPadding(
-                      padding: EdgeInsets.only(left: 15, top: 10),
+                      padding: EdgeInsets.symmetric(horizontal: 15),
                       sliver: SliverToBoxAdapter(
                         child: Text("Paid",
                             style: Theme.of(context).textTheme.headline5),
@@ -102,10 +163,47 @@ class OweMePage extends StatelessWidget {
                           childCount: oweMe.statePaid.length,
                         ),
                       ),
-                    )
-                  ]
+                    ),
+                    if (oweMe.statePaid.length > 1)
+                      ..._getHandDrawnTotal(oweMe.statePaid),
+                  ],
+                  SliverYomSpacer(
+                    height: 10,
+                  )
                 ],
               )
             : OweMePageEmptyState());
+  }
+}
+
+class HandDrawnLinePainter extends CustomPainter {
+  final double width;
+  HandDrawnLinePainter({@required this.width});
+  @override
+  void paint(Canvas canvas, Size size) {
+    YomDesign yomDesign = YomDesign();
+    DrawConfig myDrawConfig =
+        DrawConfig.build(roughness: 3, maxRandomnessOffset: 3);
+
+    FillerConfig myFillerConfig = FillerConfig.defaultConfig;
+    Filler myFiller = NoFiller(myFillerConfig);
+    Generator generator = Generator(
+      myDrawConfig,
+      myFiller,
+    );
+    Drawable figure = generator.line(size.width - width, 0, size.width, 0);
+    canvas.drawRough(
+        figure,
+        Paint()
+          ..color = yomDesign.yomGrey1
+          ..strokeWidth = 3
+          ..style = PaintingStyle.stroke
+          ..strokeCap = StrokeCap.round,
+        Paint()..color = Colors.redAccent);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
   }
 }
