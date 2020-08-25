162ccbcc222eea4e90bfd0b4d05c86bc3b2ee082
Preet Parekh <preetjdp123@gmail.com>
2020-08-25 20:02:41 +0530

    Implement a Settings Page

--- mobileApp/lib/ui/SettingsPage/settingsPage.dart
+++ mobileApp/lib/ui/SettingsPage/settingsPage.dart
@@ -1,3 +1,8 @@
+import 'package:YouOweMe/resources/notifiers/meNotifier.dart';
+import 'package:YouOweMe/ui/Abstractions/yomAppBar.dart';
+import 'package:YouOweMe/ui/Abstractions/yomAvatar.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -5,22 +10,46 @@ import 'package:package_info/package_info.dart';
 import 'package:settings_ui/settings_ui.dart';
 import 'package:YouOweMe/resources/providers.dart';
 import 'package:YouOweMe/resources/notifiers/devicePreviewSetting.dart';
+import 'package:YouOweMe/resources/extensions.dart';
 
 class SettingsPage extends HookWidget {
+  void logOut() {
+    FirebaseAuth.instance.signOut();
+  }
+
   @override
   Widget build(BuildContext context) {
     final AsyncValue<PackageInfo> packageInfo =
         useProvider(packageInfoProvider);
     final devicePreviewSetting =
         useProvider(devicePreviewSettingProvider.state);
+    final MeNotifier meNotifier = useProvider(meNotifierProvider);
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Settings",
-        ),
+      appBar: YomAppBar(
+        title: "Settings",
       ),
       body: SettingsList(
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
         sections: [
+          CustomSection(
+            child: Padding(
+              padding: EdgeInsets.all(15).copyWith(bottom: 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  YomAvatar(
+                    text: meNotifier?.me?.shortName ?? "PP",
+                    size: 100,
+                  ),
+                  Text(
+                    meNotifier.me.name,
+                    style: Theme.of(context).textTheme.headline5,
+                  )
+                ],
+              ),
+            ),
+          ),
           SettingsSection(
             title: 'General',
             tiles: [
@@ -31,15 +60,15 @@ class SettingsPage extends HookWidget {
                 onTap: () {},
               ),
               SettingsTile.switchTile(
-                title: 'Sound Effects',
-                leading: Icon(Icons.surround_sound),
-                switchValue: true,
+                title: 'Shake to Help',
+                leading: Icon(Icons.screen_rotation),
+                switchValue: false,
                 onToggle: (bool value) {},
               ),
               SettingsTile.switchTile(
-                title: 'In Game Music',
-                leading: Icon(Icons.music_note),
-                switchValue: false,
+                title: 'Notifications',
+                leading: Icon(Icons.notifications),
+                switchValue: true,
                 onToggle: (bool value) {},
               ),
             ],
@@ -53,15 +82,31 @@ class SettingsPage extends HookWidget {
                 onTap: () {},
               ),
               SettingsTile(
-                title: 'Privacy',
-                leading: Icon(Icons.privacy_tip),
+                title: 'Link with UPI',
+                subtitle: "Requires Attention",
+                leading: Icon(Icons.account_balance_wallet),
                 onTap: () {},
               ),
               SettingsTile(
-                title: 'Blocking',
-                leading: Icon(Icons.block),
+                title: 'Your Data with Us',
+                leading: Icon(Icons.analytics),
                 onTap: () {},
               ),
+              SettingsTile(
+                title: 'Logout',
+                leading: Text("😪"),
+                onTap: logOut,
+              ),
+            ],
+          ),
+          SettingsSection(
+            title: 'Miscellaneous',
+            tiles: [
+              SettingsTile(
+                  title: 'Terms of Service', leading: Icon(Icons.description)),
+              SettingsTile(
+                  title: 'Open source licenses',
+                  leading: Icon(Icons.collections_bookmark)),
             ],
           ),
           SettingsSection(
@@ -75,14 +120,6 @@ class SettingsPage extends HookWidget {
                   context.read(devicePreviewSettingProvider).toggle();
                 },
               ),
-              // SettingsTile.switchTile(
-              //   title: 'Enable UI Placeholders',
-              //   leading: Icon(Icons.carpenter),
-              //   switchValue: placeholderSetting,
-              //   onToggle: (bool value) {
-              //     context.read(placeholderSettingProvider).toggle();
-              //   },
-              // ),
             ],
           ),
           CustomSection(
@@ -93,7 +130,9 @@ class SettingsPage extends HookWidget {
                           data: (packageInfo) =>
                               Text("Version ${packageInfo.version}"),
                           loading: () => Text("Loading"),
-                          error: (a, b) => Text("Error")))))
+                          error: (a, b) => Text(
+                                "Error",
+                              )))))
         ],
       ),
     );
